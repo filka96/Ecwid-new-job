addbbe4d613a550d047cf711d5496f1d64dc337f
Filipp Semenkov <filipp.semenkov@lightspeedhq.com>
2023-03-15 14:25:15 +0400

    added HyperLogLog info

--- src/com/ecwid/newjob/ipcounter/Main.java
+++ src/com/ecwid/newjob/ipcounter/Main.java
@@ -2,6 +2,10 @@ package com.ecwid.newjob.ipcounter;
 
 import java.util.function.Function;
 
+/** если нужна приблизительная точность можно использовать
+    <a href="https://en.wikipedia.org/wiki/HyperLogLog">HyperLogLog</a>
+ */
+
 public class Main {
     public static void main(String[] args) {
         var filePath = "ip_addresses";
